f79eda2a55c0b19ce686d9670cd1e6ec8290ac68
Atul Tingre <atultingre.work@gmail.com>
2024-05-13 17:50:02 +0530

    dashboard image added

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <section className="bg-gray-50 flex items-center flex-col">
+      <section className="bg-gray-50 flex items-center flex-col m-7">
         <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex ">
           <div className="mx-auto max-w-xl text-center">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
@@ -30,7 +30,7 @@ const Hero = () => {
           </div>
         </div>
         <Image
-          src="./logo.svg"
+          src="/dashboard.png"
           alt="dashboard"
           width={1000}
           height={700}
